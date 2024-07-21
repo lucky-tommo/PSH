50a7535d2d8071457f02d2afb23fe1246a71a6f2
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-07-21 13:59:08 +1000

    Update getbitlockerrecoverykeys.ps1

--- getbitlockerrecoverykeys.ps1
+++ getbitlockerrecoverykeys.ps1
@@ -1,3 +1,11 @@
+#Powershell Script to query all machine objects in Active Directory
+#Check for presence of Bitlocker Recovery information
+#and Grab the plain text Bitlocker Recovery password. 
+#Outputs to CSV on Desktop. 
+ 
+
+$Results=@()
+$Results += "Hostname" + "," + "Bitlocker Password"
 $computers = Get-ADComputer -Filter * -Property *
 
 Foreach($machine in $computers)
@@ -9,8 +17,13 @@ Foreach($machine in $computers)
     $ADObjects += New-Object -TypeName PSObject -Property $Properties 
     
     Write-Host "Computer: "$machine.DNSHostName " -> AD Key Count:" $Bitlocker_Object.'msFVE-RecoveryPassword'.Count " -> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
-   
+    $Results += $machine.DNSHostname + "," + $Bitlocker_Object.'msFVE-RecoveryPassword'}
     #If multiple keys are present in your environment - use the below to get Key ID
     #Write-Host "Computer: "$machine.DNSHostName " -> Bitlocker Key ID:" $Bitlocker_Object.'msFVE-RecoveryGUID'" --> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
-}
 
+
+#Output to CSV
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$DesktopPath = $DesktopPath.toString() + "\BitlockerPasswords.csv"
+
+write-output $Results > $DesktopPath
