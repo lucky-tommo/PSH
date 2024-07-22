f70918cf6eca334fd9f9f1d66b2cbef6ef95e5a6
lucky-tommo <141331999+lucky-tommo@users.noreply.github.com>
2024-07-22 12:32:43 +1000

    Update getbitlockerrecoverykeys.ps1
    changed from $machine.dnsHostName to $machine.Name to facilitate easier quierying within Crowdstrike NGSIEM

--- getbitlockerrecoverykeys.ps1
+++ getbitlockerrecoverykeys.ps1
@@ -16,16 +16,16 @@ Foreach($machine in $computers)
  
     $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $machine.DistinguishedName -Properties 'msFVE-RecoveryPassword'
     
-    $Properties = @{'HostName'=$machine.DNSHostName;  'BitLockerInfo'=$Bitlocker_Object}
+    $Properties = @{'HostName'=$machine.Name;  'BitLockerInfo'=$Bitlocker_Object}
     $ADObjects += New-Object -TypeName PSObject -Property $Properties 
     
-    Write-Host "Computer: "$machine.DNSHostName " -> AD Key Count:" $Bitlocker_Object.'msFVE-RecoveryPassword'.Count " -> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
-    $Results += $machine.DNSHostname + "," + $Bitlocker_Object.'msFVE-RecoveryPassword'}
+    Write-Host "Computer: "$machine.Name " -> AD Key Count:" $Bitlocker_Object.'msFVE-RecoveryPassword'.Count " -> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
+    $Results += $machine.Name + "," + $Bitlocker_Object.'msFVE-RecoveryPassword'}
     
     
     #If multiple keys are present in your environment - use the below to get Key ID
-    #Write-Host "Computer: "$machine.DNSHostName " -> Bitlocker Key ID:" $Bitlocker_Object.'msFVE-RecoveryGUID'" --> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
-    #$Results += $machine.DNSHostname + "," + $Bitlocker_Object.'msFVE-RecoveryPassword' + "," + $bitlocker_Object.'msFVE-RecoveryGUID'
+    #Write-Host "Computer: "$machine.Name " -> Bitlocker Key ID:" $Bitlocker_Object.'msFVE-RecoveryGUID'" --> Bitlocker Recovery Keys:" $Bitlocker_Object.'msFVE-RecoveryPassword'""
+    #$Results += $machine.Name + "," + $Bitlocker_Object.'msFVE-RecoveryPassword' + "," + $bitlocker_Object.'msFVE-RecoveryGUID'
 
 #Output to CSV
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
